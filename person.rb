cd8485abd751acd0304ed67173b5ef07273ef5db
Alibaba2023 <126192888+Alibaba2023@users.noreply.github.com>
2023-08-29 16:55:46 +0430

    linter error fixed

--- person.rb
+++ person.rb
@@ -1,25 +1,25 @@
 class Person
-  attr_accessor :id, :name, :age, :parent_permission
+  attr_accessor :id, :name, :age, :parent_permession
 
-  def initialize(age, name: 'unknown', parent_permission: true)
-    @id = rand(1..1000)
+  def initialize(age, name = 'unkown', parent_permession: true)
+    @id = rand
     @name = name
-    @parent_permission = parent_permission
+    @parent_permession = parent_permession
     @age = age
+  end
+
+  def can_use_services?
+    @age >= 18 || @parent_permession
   end
 
   private
 
-  def is_age?
+  def of_age?
     @age >= 18
-  end
-
-  def can_use_services?
-    @age >= 18 || @parent_permission
   end
 end
 
-person = Person.new(25)
-person.name = 'Ali'
-person.parent_permission = true
+person = Person.new(13)
+person.name = 'person'
+person.parent_permession = true
 puts person
