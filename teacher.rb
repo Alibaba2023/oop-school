616a04450a332c2784bfc191f1829e2dad03e2cc
Alibaba2023 <126192888+Alibaba2023@users.noreply.github.com>
2023-09-01 10:06:14 +0430

    Basic UI added

--- teacher.rb
+++ teacher.rb
@@ -1,8 +1,8 @@
 require_relative 'person'
 
 class Teacher < Person
-  def initialize(name, age, specialization, permission: true)
-    super(name, age, permission)
+  def initialize(name, age, specialization, parent_permission: true)
+    super(name, age, parent_permission: parent_permission)
     @specialization = specialization
   end
 
